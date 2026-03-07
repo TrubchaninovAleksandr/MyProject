729df1cd96fbf40e25c9ed37975717a019b78756
aleksandrtrubcaninov1984@gmail.com <aleksandrtrubcaninov1984@gmail.com>
2026-03-08 00:32:26 +0300

    commit003

--- MyProject/internal/adapters/storage.go
+++ MyProject/internal/adapters/storage.go
@@ -1,6 +1,8 @@
 package adapters
 
 import (
+	"MyProject/internal/entities"
+	"context"
 	"database/sql"
 )
 
@@ -10,4 +12,18 @@ type PostgresStorage struct {
 
 func NewPostgresStorage(db *sql.DB) *PostgresStorage {
 	return &PostgresStorage{db: db}
+}
+
+// получение названия монет из БД
+func (s *PostgresStorage) GetTitles(ctx context.Context) ([]string, error) {
+}
+
+// сохраняем монеты в БД
+func (s *PostgresStorage) StoreCoins(ctx context.Context, coins []entities.Coin) error {
+
+}
+
+// получаем монеты по названиям из БД
+func (s *PostgresStorage) GetCoins(ctx context.Context, titles []string) ([]entities.Coin, error) {
+
 }
